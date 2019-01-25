e3e1def17f0994e076eef920afadcb5787520c83
Omar Rasool <webdevromar@gmail.com>
2019-01-25 17:04:04 -0600

    Move nav to component, add vars via import, etc

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -5,39 +5,12 @@
     </div>
     <div class="column">
       <div class="container">
-        <nav class="level">
-          <div class="level-left">
-            <div class="level-item"></div>
-          </div>
-          <div class="level-right">
-            <div class="level-item">
-              <div class="social--icon">
-                <a href="https://twitter.com/whimandwind" target="_blank">
-                  <eva-icon name="twitter" animation="flip" fill="#3596FD"></eva-icon>
-                </a>
-              </div>
-              <div class="social--icon">
-                <a href="https://github.com/omarrasool/omar" target="_blank">
-                  <eva-icon name="github" animation="flip" fill="#3596FD"></eva-icon>
-                </a>
-              </div>
-              <a
-                href="https://twitter.com/whimandwind"
-                target="_blank"
-                class="button"
-                role="button"
-              >Say Hello</a>
-            </div>
-          </div>
-        </nav>
+        <Nav/>
 
         <section class="section pt0">
           <div class="container">
             <h1 class="is-size-4-mobile">{{getGreeting}}, I'm Omar</h1>
-            <h2
-              class="is-size-5-mobile"
-            >A Developer | UX Designer | Indie Maker from the United States.</h2>
-            <p>I make useful, interesting things, sometimes more interesting than useful. This website is a personal log of my work and my thoughts as a human. You can view some of my projects below.</p>
+            <p>A Developer, UX Designer, and Indie Maker from the United States. I like to organize complex features into simple user experiences. In my free time, I make useful, interesting things, sometimes more interesting than useful. This website is a personal log of my work and my thoughts as a human. Feel free to Say Hello.</p>
           </div>
         </section>
 
@@ -48,7 +21,7 @@
           <div class="columns">
             <div class="column">
               <h4 class="project--title">
-                <a class="project--link" href="#">Color Koala</a>
+                <a class="project--link" href="https://colorkoala.xyz/" target="_blank">ColorKoala.xyz</a>
               </h4>
               <p>Koala-ty color palette generator, gorgeous colors for all of your side projects.</p>
             </div>
@@ -61,15 +34,12 @@
           <hr class="hr">
           <div class="columns">
             <div class="column is-12">
-              <span class="text-top"><eva-icon name="image-2" animation="flip" fill="#3596FD"></eva-icon></span>
+              <span class="text-top"><eva-icon name="image-2" animation="none" fill="#3596FD"></eva-icon></span>
               <strong> 41 cities</strong> &nbsp;
-              <span class="text-top"><eva-icon name="globe-2" animation="flip" fill="#3596FD"></eva-icon></span>
+              <span class="text-top"><eva-icon name="globe-2" animation="none" fill="#3596FD"></eva-icon></span>
               <strong> 16 countries</strong> &nbsp;
-               <span class="text-top"><eva-icon name="heart" animation="flip" fill="#3596FD"></eva-icon></span>
+               <span class="text-top"><eva-icon name="heart" animation="none" fill="#3596FD"></eva-icon></span>
               <strong> {{getSteps}} steps</strong>
-              <div class=""><br>
-                Montreal, Quebec City, Magog, Sherbrooke, Stanstead, Coaticook • Taipei, Taichung • Osaka, Kyoto, Nagoya, Tokyo • Bangkok, Chiang Mai, Pai, Koh Phangan • Fes, Marrakesh, Rabat, Essaouira • Tenerife, Lanzarote, La Gran Canaria • Dubrovnik, Zadar, Sukosan • Budva, Podgorica • Budapest • Prague • Lüchow, Berlin, Hamburg, Frankfurt, Leipzig • Amsterdam • Ostend, Brugge, Antwerp • Barcelona • Paris, Versailles • London
-              </div>
             </div>
             <div class="column">
               
@@ -82,9 +52,14 @@
 </template>
 
 <script>
+import Nav from '@/components/UI/Nav.vue'
+
 export default {
   name: "HomePage",
   props: {},
+  components: {
+    Nav
+  },
   computed: {
     getGreeting() {
       const currentTime = new Date().getHours();
@@ -95,7 +70,7 @@ export default {
       } else if (currentTime >= 17 && currentTime <= 24) {
         return "Good Evening";
       } else {
-        return "Hello";
+        return "Hi there";
       }
     },
     getSteps() {
@@ -106,31 +81,13 @@ export default {
 };
 </script>
 
-<style lang="sass" scoped>
-@import url('https://fonts.googleapis.com/css?family=Roboto+Mono:700');
-// COLORS
-$prime: #3596FD
-$second: #140C36
+<style lang="sass">
+@import url('https://fonts.googleapis.com/css?family=Roboto+Mono:700')
+@import '@/styles/vars.sass'
 
 .section
-  padding-bottom: 0
+  padding: 1.5rem
 
-.button
-  margin-top: 25px
-  margin-right: 25px
-  border: 2px solid $prime
-  font-size: 22px
-  border-radius: 100px
-  font-weight: bold
-  background-color: transparent
-  color: $prime
-  opacity: .5
-  transition: all 200ms ease-in-out
-
-  &:hover
-    opacity: 1
-    border: 2px solid $prime
-    color: $prime
 
 .section--title
   font-size: 18px
@@ -143,7 +100,6 @@ $second: #140C36
   margin: 5px 0 20px 0
   opacity: 0.3
 
-
 .project--title
   font-family: 'Roboto Mono', monospace
   font-weight: bold
@@ -151,17 +107,12 @@ $second: #140C36
 
 .graphic
   height: 100vh
-  position: relative
   background: url('../assets/o-graphic.svg')
+  position: relative
   background-repeat: no-repeat
   background-size: cover
   background-position: center right
-  animation: Gradient 5s ease infinite
+  opacity: .7
 
-.social--icon
-  padding: 30px 10px 0 5px
-  opacity: .5
-  &:hover
-    opacity: 1
 </style>
 
---
+++ src/components/UI/Nav.vue
@@ -0,0 +1,60 @@
+<template>
+  <nav class="level">
+    <div class="level-left">
+      <div class="level-item"></div>
+    </div>
+    <div class="level-right">
+      <div class="level-item">
+        <div class="social--icon">
+          <a href="https://twitter.com/whimandwind" target="_blank">
+            <eva-icon name="twitter" animation="pulse" fill="#3596FD"></eva-icon>
+          </a>
+        </div>
+        <div class="social--icon">
+          <a href="https://github.com/omarrasool/omar" target="_blank">
+            <eva-icon name="github" animation="pulse" fill="#3596FD"></eva-icon>
+          </a>
+        </div>
+        <a
+          href="https://twitter.com/whimandwind"
+          target="_blank"
+          class="button"
+          role="button"
+        >Say Hello</a>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Nav"
+};
+</script>
+
+<style lang="sass">
+@import '@/styles/vars.sass'
+
+.button
+  margin-top: 25px
+  margin-right: 25px
+  border: 2px solid $prime
+  font-size: 22px
+  border-radius: 100px
+  font-weight: bold
+  background-color: transparent
+  color: $prime
+  opacity: .5
+  transition: all 200ms ease-in-out
+
+  &:hover
+    opacity: 1
+    border: 2px solid $prime
+    color: $prime
+
+.social--icon
+  padding: 30px 10px 0 5px
+  opacity: .5
+  &:hover
+    opacity: 1
+</style>
