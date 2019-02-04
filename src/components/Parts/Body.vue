8e9a11abfee9c233f41560770ef705c0e8c1da58
Omar Rasool <webdevromar@gmail.com>
2019-02-04 10:32:20 -0600

    update copy

--- src/components/Parts/Body.vue
+++ src/components/Parts/Body.vue
@@ -2,7 +2,7 @@
     <section class="section pt0">
       <div class="container">
         <h1 class="is-size-3-mobile">Hello, I'm Omar</h1>
-        <p>An award winning Developer, UX Designer, from the U.S. I enjoy organizing complex features into simple user experiences. I've been fortunate enough to work on projects for Purina, Monsanto, MamaLola, Sprint, Anheuser Busch, and Chris Long. In my free time I make useful and interesting things. This website is a personal log of my work, If you'd like to work together on a project feel free to <a href="mailto:webdevromar@gmail.com?Subject=%20Hello" class="" role="button">Say Hello</a></p>
+        <p>An award winning Developer, UX Designer, and tinkerer from the U.S. I enjoy organizing complex features into simple user experiences. I've been fortunate enough to work on projects for Purina, Monsanto, MamaLola, Sprint, Anheuser Busch, and Chris Long. In my free time I make useful and interesting things. This website is a personal log of my work, If you'd like to work together on a project feel free to <a href="mailto:webdevromar@gmail.com?Subject=%20Hello" class="" role="button">Say Hello</a></p>
       </div>
     </section>
 </template>
