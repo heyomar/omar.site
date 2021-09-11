64e1df824224acc581ac6d7a639be96c19791f76
heyomar <25757036+heyomar@users.noreply.github.com>
2021-09-11 15:58:32 -0500

    rip out old vue-cli project, replace with vite

---
+++ App.vue
@@ -0,0 +1,44 @@
+<template>
+    <div id="app">
+        <router-view />
+    </div>
+</template>
+
+<style lang="scss">
+    @import url("https://fonts.googleapis.com/css?family=Roboto+Mono:700");
+    @import "bulma";
+    @import "@/styles/vars.scss";
+    @import "@/styles/base.scss";
+
+    .section {
+        padding: 1.5rem 1.5rem 2.8rem 1.5rem;
+    }
+
+    .section--title {
+        font-size: 15px;
+    }
+
+    .hr {
+        background-color: $prime;
+        border: none;
+        display: block;
+        height: 1px;
+        margin: 5px 0 20px 0;
+        opacity: 0.3;
+    }
+
+    .project--title {
+        font-family: "Roboto Mono", monospace;
+        font-weight: bold;
+        font-size: 25px;
+    }
+
+    .graphic {
+        height: 100vh;
+        background: url("assets/o-graphic.svg");
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-position: center right;
+    }
+</style>
+
---
+++ main.js
@@ -0,0 +1,19 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
+
+
+
+// import Vue from 'vue'
+// import App from './App.vue'
+// import router from './router'
+// import EvaIcons from 'vue-eva-icons'
+
+// Vue.use(EvaIcons)
+// Vue.config.productionTip = false
+
+// new Vue({
+//     router,
+//     render: h => h(App)
+// }).$mount('#app')
