905a6a6ded75370096488eafeed55fe948bb361b
heyomar <25757036+heyomar@users.noreply.github.com>
2021-09-12 08:32:33 -0500

    remove eva icons

--- src/main.js
+++ src/main.js
@@ -3,7 +3,5 @@ import App from './App.vue'
 import router from './router/index'
 import './styles/app.css'
 
-import EvaIcons from 'vue-eva-icons'
-Vue.use(EvaIcons)
 
 createApp(App).use(router).mount('#app')
